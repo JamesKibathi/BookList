44864181817b13b772b4523f46382b7c4a81e870
James Njenga <knjenga94@gmail.com>
2024-01-23 19:11:29 +0300

    Implement class based views to get all books

--- BookListAPI/urls.py
+++ BookListAPI/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/',views.books)
+    # path('books/',views.books) - functional based
+    path('books',views.BookList.as_view)
 ]
--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
 # Create your views here.
-@api_view()
+@api_view(['GET','POST'])
 def books(request):
     return Response("List of the books", status=status.HTTP_200_OK)
+
+class BookList(APIView):
+    def get(self,request):
+        return Response({"message":"List of books"},status.HTTP_200_OK)
